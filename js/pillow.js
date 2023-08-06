a0d221c12e804d2836ed8f3f30cdfd0c2f8df6d5
deezertidal <jrull5400@gmail.com>
2023-08-06 09:20:58 +0800

    Update pillow.js

--- js/pillow.js
+++ js/pillow.js
@@ -1,32 +1,40 @@
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
+var obj = JSON.parse($response.body);
 
-const vip1 = '/apps';
-/***const pay1 = '/abtest';***/
-/**const pay2 = '/ucapp/getUserSVip';**/
-
-if (url.indexOf(vip1) != -1) {
-    obj = {
-  "receive_receipts_enable" : true,
-  "uses_provisional_auth" : true,
-  "outcomes" : {
-    "direct" : {
-      "enabled" : true
-    },
-    "indirect" : {
-      "enabled" : true,
-      "notification_attribution" : {
-        "limit" : 10,
-        "minutes_since_displayed" : 60
+obj = {
+  "request_date": "2022-08-06T02:30:14Z",
+  "request_date_ms": 1837536263000,
+  "subscriber": {
+    "entitlements": {
+      "premium": {
+        "expires_date": "2023-08-06T02:30:14Z",
+        "grace_period_expires_date": null,
+        "product_identifier": "com.neybox.pillow.premium.month",
+        "purchase_date": "2022-08-06T02:30:14Z"
       }
     },
-    "unattributed" : {
-      "enabled" : true
+    "first_seen": "2022-08-06T02:30:14Z",
+    "last_seen": "2022-08-06T02:30:14Z",
+    "management_url": null,
+    "non_subscriptions": {},
+    "original_app_user_id": "YOUR_USER_ID",
+    "original_application_version": "YOUR_APP_VERSION",
+    "original_purchase_date": "2022-08-06T02:30:14Z",
+    "other_purchases": {},
+    "subscriptions": {
+      "com.neybox.pillow.premium.month": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2099-08-06T02:30:14Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2022-08-06T02:30:14Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "active",
+        "purchase_date": "2022-08-06T02:30:14Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": "2022-09-08T02:30:14Z"
+      }
     }
   }
-}
-     body = JSON.stringify(obj);
-}
+};
 
-$done({body});
+$done({ "body": JSON.stringify(obj) });
